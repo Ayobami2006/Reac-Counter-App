55f3ec9f5a652802af8339918bff0fa226b422ed
Ayobami2006 <mujeebayo20@gmail.com>
2022-11-01 11:12:25 +0100

    first commit

---
+++ src/script.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function Hello() {
+//     return (
+//         <div className="App">
+//           <header className="App-header">
+//             <img src={logo} class="width" alt="logo" />
+//             <p>
+//               Hello world
+//             </p>
+//           </header>
+//         </div>
+//       );
+// }
+// export default Hello;
